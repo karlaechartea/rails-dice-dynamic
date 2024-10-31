ae3ffd5a11403277245be8e3fd6a377e8f04be84
karlaechartea <k.echartea@chicagobooth.edu>
2024-10-31 00:51:45 +0000

    save

---
+++ app/controllers/game_controller.rb
@@ -0,0 +1,45 @@
+class GameController < ApplicationController
+  def homepage
+      render({ :template => "game_templates/homepage" })
+      end
+
+  def two_six
+    @first_dice = rand(1..6)
+    @second_dice = rand(1..6)
+
+    render({ :template => "game_templates/2d6" })
+  end
+
+  def two_ten
+      @first_dice = rand(1..10)
+      @second_dice = rand(1..10)
+  
+      render({ :template => "game_templates/2d10" })
+  end
+
+  def one_twenty
+    @first_dice = rand(1..10)
+
+    render({ :template => "game_templates/1d20" })
+  end
+
+  def five_four
+    @first_dice = rand(1..4)
+    @second_dice = rand(1..4)
+    @third_dice = rand(1..4)
+    @fourth_dice = rand(1..4)
+    @fifth_dice = rand(1..4)
+
+    render({ :template => "game_templates/5d4" })
+  end
+
+  def flexible
+  @num_dice = params[:random_dice].to_i
+  @sides = params[:random_sides].to_i
+  @rolls = Array.new(@num_dice) { rand(1..@sides) }
+
+  render({ :template => "game_templates/flexible" })
+ 
+  end
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,13 @@
 Rails.application.routes.draw do
+  get("/", { :controller => "game", :action => "homepage" })
 
+  get("/dice/2/6", { :controller => "game", :action => "two_six" })
+
+  get("/dice/2/10", { :controller => "game", :action => "two_ten" })
+
+  get("/dice/1/20", { :controller => "game", :action => "one_twenty" })
+
+  get("/dice/5/4", { :controller => "game", :action => "five_four" })
+
+  get("/dice/:random_dice/:random_sides", { :controller => "game", :action => "flexible" })
 end
